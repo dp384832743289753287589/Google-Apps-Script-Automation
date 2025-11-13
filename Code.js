d17d430a01ba5d2a2643290a9c3db3886055fe79
unknown <darsheagles@gmail.com>
2025-11-13 16:40:18 -0500

    Fixing Error

--- Code.js
+++ Code.js
@@ -18,7 +18,7 @@ function myFunction() {
   
   for (let i=0;i<c.length;i++){
     let yes_message="Hello "+contact_names[i]+" \nWe are pleased that you have accepted our sponsorship proposal on behalf of "+business_names[i]+". We would like to emphasize the impact that your contribution would have within the local community as a sponsor of our team. However, we respect your decision.\nThanks again for considering the offer!\nRegards,Team 2554"
-    let no_message="Hello "+contact_names[i]+"\n We regret to hear that CVS Pharmacy has declined our offer for sponsorship. We would like to emphasize the impact that your contribution would have within the local community as a sponsor of our team. However, we respect your decision. Thanks again for considering the offer!\n Team 2554"
+    let no_message="Hello "+{contact_names[i]+"\n We regret to hear that "+business_names[i]"has declined our offer for sponsorship. We would like to emphasize the impact that your contribution would have within the local community as a sponsor of our team. However, we respect your decision. Thanks again for considering the offer!\n Team 2554"
  
        
       
